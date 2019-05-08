3781d66f671f535ca31d2295cb9d9817e52ab851
stajics <stajic.srdjan92@gmail.com>
2019-05-08 16:54:15 +0200

    Added jest snapshot test and enzyme unit test

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -1,14 +1,34 @@
-/**
- * @format
- */
+import ReactNative from 'react-native';
+// Note: test renderer must be required after react-native.
+import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
 
-import 'react-native';
 import React from 'react';
 import App from '../App';
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+const { View, Text, Switch } = ReactNative;
+
+describe('jest snapshot tests', () => {
+  it('renders correctly', () => {
+    const tree = renderer.create(<App />).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+
+describe('enzyme tests', () => {
+  it('should render a <App /> component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find(View)).toHaveLength(1);
+    expect(wrapper.find(Text)).toHaveLength(3);
+    expect(wrapper.find(Switch)).toHaveLength(1);
+  });
+
+  it('should togggle switch to true', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.state().isToggled).toBe(false);
+    wrapper.find(Switch).first().props().onChange();
+    expect(wrapper.state().isToggled).toBe(true);
+  });
 });
