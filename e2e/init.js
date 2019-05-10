4cfc0e8e4b3ea4509c53e96272fcb8e11b5da13c
stajics <stajic.srdjan92@gmail.com>
2019-05-11 00:38:53 +0200

    semaphore.yml changed

--- e2e/init.js
+++ e2e/init.js
@@ -1,6 +1,6 @@
 const detox = require('detox');
 const adapter = require('detox/runners/jest/adapter');
-// const specReporter = require('detox/runners/jest/specReporter');
+const specReporter = require('detox/runners/jest/specReporter');
 const config = require('../package.json').detox;
 // Set the default timeout
 jest.setTimeout(1000000);
@@ -8,7 +8,7 @@ jasmine.getEnv().addReporter(adapter);
 
 // This takes care of generating status logs on a per-spec basis. By default, jest only reports at file-level.
 // This is strictly optional.
-// jasmine.getEnv().addReporter(specReporter);
+jasmine.getEnv().addReporter(specReporter);
 
 beforeAll(async () => {
   await detox.init(config);
